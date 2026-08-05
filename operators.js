cf0db86934ef435a991084967a38970b09828dfe
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-05 14:54:07 +0530

    Day 1 Operators and Variables

---
+++ operators.js
@@ -0,0 +1,46 @@
+// //Operators
+// //Aritjemetic operators
+// let a=10;
+// let b=20;
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a%b=",a%b)
+// console.log("a**b=",a**b)
+
+// // //unary operator
+// a++;
+// b--;
+// console.log(a)
+// console.log(b)
+
+// //assignment operators
+// let c=2;
+// let d=3;
+// console.log(c+=2) 
+// console.log(d-=1)
+// console.log(c*=d)
+// console.log(d/=2)
+
+
+// //comparision operator
+// let a=5;
+// let b=3;
+// let c="3";
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(c==b)
+// console.log(a>=b)
+// console.log(a<=b)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(c===b)
+// console.log(c!==b)
+
+//logical operators
+let con1=true;
+let con2=false;
+console.log(con1&&con2)
+console.log(con1||con2)
+console.log(!con1)
---
+++ variables_dt.js
@@ -0,0 +1,28 @@
+let firstname="Deekshitha";
+console.log(firstname)
+
+
+
+let isStudent=true;
+console.log(isStudent)
+
+city="Hyderabad";
+console.log(city)
+
+const college="TKR";
+console.log(college)
+
+let score=50;
+score=75;
+console.log(score)
+
+let salary=50000;
+console.log(salary)
+console.log(typeof salary)
+
+let name = "Deekshitha";
+let age = 21;
+let isPlaced = false;
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isPlaced)
