2cb2c68a02d57931b2de6a2714bc27046d66ef4b
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-06 18:31:37 +0530

    day-2

---
+++ dailyprac.js
@@ -0,0 +1,17 @@
+var math = 85;
+var science = 90;
+var english = 78;
+let total_marks=math+science+english;
+var percentage=(total_marks/300)*100
+console.log("Total marks: ",total_marks)
+console.log("Percentage: ",percentage)
+
+//swap 2 numbers
+let a = 10;
+let b = 20;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a, b);
--- operators.js
+++ operators.js
@@ -1,42 +1,42 @@
-// //Operators
-// //Aritjemetic operators
-// let a=10;
-// let b=20;
-// console.log("a+b=",a+b)
-// console.log("a-b=",a-b)
-// console.log("a*b=",a*b)
-// console.log("a/b=",a/b)
-// console.log("a%b=",a%b)
-// console.log("a**b=",a**b)
+//Operators
+//Aritjemetic operators
+let a=10;
+let b=20;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a%b=",a%b)
+console.log("a**b=",a**b)
 
-// // //unary operator
-// a++;
-// b--;
-// console.log(a)
-// console.log(b)
+/ //unary operator
+a++;
+b--;
+console.log(a)
+console.log(b)
 
-// //assignment operators
-// let c=2;
-// let d=3;
-// console.log(c+=2) 
-// console.log(d-=1)
-// console.log(c*=d)
-// console.log(d/=2)
+//assignment operators
+let c=2;
+let d=3;
+console.log(c+=2) 
+console.log(d-=1)
+console.log(c*=d)
+console.log(d/=2)
 
 
-// //comparision operator
-// let a=5;
-// let b=3;
-// let c="3";
-// console.log(a==b)
-// console.log(a!=b)
-// console.log(c==b)
-// console.log(a>=b)
-// console.log(a<=b)
-// console.log(a>b)
-// console.log(a<b)
-// console.log(c===b)
-// console.log(c!==b)
+//comparision operator
+let a=5;
+let b=3;
+let c="3";
+console.log(a==b)
+console.log(a!=b)
+console.log(c==b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a>b)
+console.log(a<b)
+console.log(c===b)
+console.log(c!==b)
 
 //logical operators
 let con1=true;
