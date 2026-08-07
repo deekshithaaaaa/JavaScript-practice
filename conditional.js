f8d1b3cc143e9e9675c0b6704a307e5a635511e8
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-07 12:10:51 +0530

    day-3 conditional statements

--- conditional.js
+++ conditional.js
@@ -1,47 +1,47 @@
-// let mode="dark";
-// let color;
-// if(mode==="dark"){
-//     color="white";
-// }
-// else{
-//     color="black";
-// }
-// console.log(color)
+let mode="dark";
+let color;
+if(mode==="dark"){
+    color="white";
+}
+else{
+    color="black";
+}
+console.log(color)
 
-// let age=18;
-// if(age>=18){
-//     console.log("Eligible for voting")
-// }
-// else{
-//     console.log("not eligible for voting")
-// }
+let age=18;
+if(age>=18){
+    console.log("Eligible for voting")
+}
+else{
+    console.log("not eligible for voting")
+}
 
-// let num=3;
-// if(num%2===0){
-//     console.log("Even num")
-// }
-// else{
-//     console.log("Odd num")
-// }
+let num=3;
+if(num%2===0){
+    console.log("Even num")
+}
+else{
+    console.log("Odd num")
+}
 
-// let age=56;
-// if(age<18){
-//     console.log("child")
-// }
-// else if(age>60){
-//     console.log("old")
-// }
-// else{
-//     console.log("middle")
-// }
+let age=56;
+if(age<18){
+    console.log("child")
+}
+else if(age>60){
+    console.log("old")
+}
+else{
+    console.log("middle")
+}
 
-// let num=prompt("Enter a number:")
-// if(num%5===0){
-//     console.log("Multiple of five")
-// }
-// else{
-//     console.log("Not a multiple")
-// }
+let num=prompt("Enter a number:")
+if(num%5===0){
+    console.log("Multiple of five")
+}
+else{
+    console.log("Not a multiple")
+}
 
 let marks=prompt("Enter marks:")
 if(marks>100){
