9e4699261ab03c0d04d2b527735b95e90918d583
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-12 17:23:21 +0530

    practiced string  methods

---
+++ strings.js
@@ -0,0 +1,57 @@
+// let st1="Java Script";
+// console.log(st1.length)
+// console.log(st1[4])
+
+// let str = "JavaScript";
+// console.log(str.length)
+
+// let str = "JavaScript";
+// console.log(str.charAt(0))
+
+// let str = "JavaScript";
+// console.log(str.toUpperCase())
+
+// let str = "JavaScript";
+// console.log(str.toLowerCase())
+
+// let str = "Hello World";
+// console.log(str.slice(0,5))
+
+// let str = "JavaScript";
+// console.log(str.slice(4,str.length))
+
+// let str = "Hello World";
+// console.log(str.slice(5))
+
+// let str = "I love JavaScript";
+// console.log(str.slice(7))
+
+// let str = "JavaScript is easy";
+// console.log(str.slice(11))
+
+// let str = "I love programming";
+// console.log(str.slice(7,14))
+
+// let str = "JavaScript";
+// console.log(str.includes("Script"))
+
+// let str = "Hello World";
+// console.log(str.startsWith("Hello"))
+
+// let str = "Hello World";
+// console.log(str.endsWith("World"))
+
+// let str = "  Hello World  ";
+// console.log(str.trim())
+
+// let str = "Hello World";
+// console.log(str.replace("World","JavaScript"))
+
+// let str = "apple,banana,orange";
+// console.log(str.split())
+
+// let str = "JavaScript is very easy";
+// console.log(str.split(" "))
+
+let str = "JavaScript";
+console.log(str.indexOf("Script"))
