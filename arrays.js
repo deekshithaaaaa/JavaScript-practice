bf504ad54c965e837da41bd1c7f57b0157a47aba
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-14 14:40:29 +0530

    array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,109 @@
-let fruits=["Apple", "Banana", "Mango", "Orange"]
-console.log(fruits)
-console.log(fruits[2])
+// let fruits=["Apple", "Banana", "Mango", "Orange"]
+// console.log(fruits)
+// console.log(fruits[2])
+// for (let i=1;i<=fruits.length;i++){
+//     console.log(i)
+// }
+
+// let arr=[85,97,44,37,76,90]
+// let sum=0
+
+// for (let i=0;i<arr.length;i++){
+//     sum=sum+arr[i]
+// }
+// let avg=sum/arr.length
+// console.log(avg)
+
+// let fruits=["Apple", "Banana", "Mango", "Orange"]
+// console.log(fruits)
+// console.log(fruits[2])
+
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers[0])
+// console.log(numbers[4])
+
+// let colors = ["Red", "Blue", "Green", "Yellow", "Pink"];
+// console.log(colors[2],colors[4])
+
+// let fruits = ["Apple", "Banana", "Mango"];
+// fruits[1]="Grapes"
+// console.log(fruits)
+
+// let numbers = [10, 20, 30, 40, 50, 60];
+// console.log(numbers.length)
+
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers[numbers.length-1])
+
+// let numbers = [10, 20, 30, 40, 50];
+// numbers[0]=100
+// numbers[4]=500
+// console.log(numbers)
+
+// let numbers = [10, 20, 30, 40, 50];
+// for (let num of numbers){
+//     console.log(num)
+// }
+
+// let numbers = [5, 10, 15, 20, 25];
+// for (let num of numbers){
+//     if (num%2===0){
+//         console.log(num)
+//     }
+// }
+
+// let numbers = [12, 7, 18, 5, 20, 9];
+// for (let num of numbers){
+//     if (num>10){
+//         console.log(num)
+//     }
+// }
+
+// let numbers = [10, 20, 30, 40, 50];
+// let sum=0
+// for (let i of numbers){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+// let numbers = [10, 25, 8, 17, 30, 5, 12];
+// let l=0
+// for (let i in numbers){
+//     if (i>l){
+//         l=i
+//     }
+// }
+// console.log(l)
+
+// let numbers = [10, 25, 8, 17, 30, 5, 12];
+// let s=numbers[0]
+// for (i of numbers){
+//     if (i<s){
+//         s=i
+//     }
+// }
+// console.log(s)
+
+// let numbers = [10, 25, 8, 17, 30, 5, 12];
+// let count=0
+// for (i of numbers){
+//     if (i%2==0){
+//         count+=1
+//     }
+// }
+// console.log(count)
+
+// let fruits = ["Apple", "Banana", "Mango"];
+// fruits.pop("orange")
+// console.log(fruits)
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// fruits.shift()
+// console.log(fruits)
+
+// let fruit = ["Apple", "Banana", "Mango", "Orange"];
+// fruit.unshift("Grapes")
+// console.log(fruit)
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits.includes("Mango"))
